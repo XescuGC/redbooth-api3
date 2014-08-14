bd717395922b6ad3ddab030f6fe7abd531d0bc86
xescuGC <xescugil@gmail.com>
2014-08-14 03:00:15 +0200

    APP: Added the routes, constants and initialize:after

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -1,5 +1,29 @@
 this.RB = (function(Backbone, Marionette) {
   App = new Marionette.Application();
 
+  App.addRegions({
+    headerRegion: '#header-region',
+    mainRegion:   '#main-region',
+    footerRegion: '#footer-region'
+  })
+
+  App.reqres.setHandler('default:region', function() {
+    return App.mainRegion;
+  })
+
+  App.rootRoute = ''
+
+  App.origin = (function() {
+    return window.location.origin
+  })
+
+  App.on('initialize:after', function() {
+    this.startHistory();
+    var route = App.rootRoute;
+    if (!this.getCurrentRoute()) {
+      this.navigate(route, {trigger: true})
+    }
+  })
+
   return App;
 })(Backbone, Marionette);
