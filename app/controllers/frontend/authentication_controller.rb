2e97dbdef3691650df63d36c32ad3acdbae02c0b
xescuGC <xescugil@gmail.com>
2014-08-20 19:57:15 +0200

    CONTROLLERS: Implemented the response of the Redbooth::Rest::Authentication and cookies setter

--- app/controllers/frontend/authentication_controller.rb
+++ app/controllers/frontend/authentication_controller.rb
@@ -1,6 +1,9 @@
 class Frontend::AuthenticationController < Frontend::ApplicationController
 
   def auth_code
+    authentication = Redbooth::Rest::Authentication.get_access_token(code: params[:code])
+    cookies.signed[:rb_t] = {value: authentication.access_token, expires: authentication.expires_in.to_i.seconds.from_now}
+    redirect_to :root
   end
 
 end
