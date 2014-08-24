c77e5f4d9c1f5bf95380420118bb27037141b278
xescuGC <xescugil@gmail.com>
2014-08-24 21:34:03 +0200

    CONTROLLERS: Application initialize the Redbooth::Rest::Util::RestObject with the :rb_t cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_auth
+
+  def set_auth
+    Rails.logger.warn("COOKIE: #{cookies.signed[:rb_t]}")
+    Redbooth::Rest::Util::RestObject::DEFAULTS[:auth] = cookies.signed[:rb_t]
+  end
 end
