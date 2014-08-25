6d454f938806c8448538e3058253af70179c366f
xescuGC <xescugil@gmail.com>
2014-08-25 03:09:44 +0200

    APP: Removed TaskListApp

--- app/assets/javascripts/backbone/apps/task_list/index/index_view.js
+++
@@ -1,5 +0,0 @@
-RB.module('TaskListApp.Index', function(Index, App, Backbone, Marionette, $, _) {
-  Index.TaskList = App.Views.ItemView.extend({
-    template: 'task_list/index/templates/task_list'
-  })
-})
