f91ce0be3a89b62203f22f0e33e3ba19c73e60ba
xescuGC <xescugil@gmail.com>
2014-08-31 14:18:58 +0200

    VIEWS: Added Flahs Alerts for error when the user don't accept the Redbooth Login

--- app/controllers/frontend/authentication_controller.rb
+++ app/controllers/frontend/authentication_controller.rb
@@ -1,9 +1,14 @@
 class Frontend::AuthenticationController < Frontend::ApplicationController
 
   def auth_code
-    authentication = Redbooth::Rest::Authentication.get_access_token(code: params[:code])
-    cookies.signed[:rb_t] = {value: authentication.access_token, expires: authentication.expires_in.to_i.seconds.from_now}
-    redirect_to :root
+    if params[:error]
+      flash[:danger] = 'You cancelled the Login, if you want to access you should accept, we won\'t save your information'
+      redirect_to :user_login
+    else
+      authentication = Redbooth::Rest::Authentication.get_access_token(code: params[:code])
+      cookies.signed[:rb_t] = {value: authentication.access_token, expires: authentication.expires_in.to_i.seconds.from_now}
+      redirect_to :root
+    end
   end
 
 end
