60b84c6d184a00474c8378a8757ede3065c8a79b
xescuGC <xescugil@gmail.com>
2014-09-02 19:11:34 +0200

    APPS: Project.Index changed the load order of the parts

--- app/assets/javascripts/backbone/apps/project/index/index_controller.js
+++ app/assets/javascripts/backbone/apps/project/index/index_controller.js
@@ -4,12 +4,12 @@ RB.module('ProjectApp.Index', function(Index, App, Backbone, Marionette, $, _) {
       var that = this;
       this.layout = this.getLayoutView();
       var promiseProjects = App.request('project:entities');
-      $.when(promiseProjects).done(function(projects) {
-        that.listenTo(that.layout, 'show', function() {
+      this.listenTo(this.layout, 'show', function() {
+        $.when(promiseProjects).done(function(projects) {
           that.projectListRegion(projects);
         })
-        that.show(that.layout);
       })
+      this.show(this.layout);
     },
 
     projectListRegion: function(projects) {
